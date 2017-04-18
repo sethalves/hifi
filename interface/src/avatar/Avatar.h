bad32a8e51e303c7c9055b7b5c3e8d5c421de3c7
Andrew Meadows <andrew@highfidelity.io>
2017-04-18 16:35:32 -0700

    repair bad merge during rebase

--- interface/src/avatar/Avatar.h
+++ interface/src/avatar/Avatar.h
@@ -335,7 +335,6 @@ protected:
     RateCounter<> _skeletonModelSimulationRate;
     RateCounter<> _jointDataSimulationRate;
 
-<<<<<<< 4318cce04a59543d80a9364c86aab79408dcb50e
     // Smoothing data for blending from one position/orientation to another on remote agents.
     float _smoothPositionTime;
     float _smoothPositionTimer;
@@ -346,8 +345,6 @@ protected:
     glm::quat _smoothOrientationInitial;
     glm::quat _smoothOrientationTarget;
 
-=======
->>>>>>> remove Menu dependency from Avatar class
 private:
     class AvatarEntityDataHash {
     public:
