a75e5c751148dcd2b0834692887db491d3e2f93a
Seth Alves <seth.alves@gmail.com>
2017-10-20 13:50:04 -0700

    hand-controller search laser will hit non-collidable entities

--- scripts/system/controllers/controllerModules/farActionGrabEntity.js
+++ scripts/system/controllers/controllerModules/farActionGrabEntity.js
@@ -573,7 +573,7 @@ Script.include("/~/system/libraries/controllers.js");
 
         this.laserPointer = LaserPointers.createLaserPointer({
             joint: (this.hand === RIGHT_HAND) ? "_CAMERA_RELATIVE_CONTROLLER_RIGHTHAND" : "_CAMERA_RELATIVE_CONTROLLER_LEFTHAND",
-            filter: RayPick.PICK_ENTITIES | RayPick.PICK_OVERLAYS,
+            filter: RayPick.PICK_ENTITIES | RayPick.PICK_OVERLAYS | RayPick.PICK_INCLUDE_NONCOLLIDABLE,
             maxDistance: PICK_MAX_DISTANCE,
             posOffset: getGrabPointSphereOffset(this.handToController(), true),
             renderStates: renderStates,
