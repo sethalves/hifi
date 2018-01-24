73896fc4f4bcf3f481ca99f9e6a0575c3218865d
Seth Alves <seth.alves@gmail.com>
2018-01-23 16:32:16 -0800

    oops

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -71,7 +71,7 @@ int main(int argc, const char* argv[]) {
 
     qDebug() << "UserActivityLogger is enabled:" << ual.isEnabled();
 
-    if (ual) {
+    if (ual.isEnabled()) {
         auto crashHandlerStarted = startCrashHandler();
         qDebug() << "Crash handler started:" << crashHandlerStarted;
     }
