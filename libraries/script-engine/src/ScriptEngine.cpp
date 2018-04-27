f5e703b16eace665d0610396e8230cfdd5948095
Seth Alves <seth.alves@gmail.com>
2018-04-27 10:42:27 -0700

    sync

--- libraries/script-engine/src/ScriptEngine.cpp
+++ libraries/script-engine/src/ScriptEngine.cpp
@@ -236,7 +236,10 @@ void ScriptEngine::disconnectNonEssentialSignals() {
     // Ensure the thread should be running, and does exist
     if (_isRunning && _isThreaded && (workerThread = thread())) {
         connect(this, &ScriptEngine::doneRunning, workerThread, &QThread::quit);
-        connect(this, &QObject::destroyed, workerThread, &QObject::deleteLater);
+        // connect(this, &QObject::destroyed, workerThread, &QObject::deleteLater);
+        connect(this, &QObject::destroyed, [=](){
+            delete workerThread;
+        });
     }
 }
 
@@ -352,7 +355,10 @@ void ScriptEngine::runInThread() {
     // disconnectNonEssentialSignals() method
     connect(workerThread, &QThread::started, this, &ScriptEngine::run);
     connect(this, &ScriptEngine::doneRunning, workerThread, &QThread::quit);
-    connect(this, &QObject::destroyed, workerThread, &QObject::deleteLater);
+    // connect(this, &QObject::destroyed, workerThread, &QObject::deleteLater);
+    connect(this, &QObject::destroyed, [=](){
+        delete workerThread;
+    });
 
     workerThread->start();
 }
