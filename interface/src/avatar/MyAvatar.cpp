4d0b167bd65d5f1d00f1b76a24892f650690f8e5
Seth Alves <seth.alves@gmail.com>
2019-04-08 17:41:07 -0700

    fix warning

--- interface/src/avatar/MyAvatar.cpp
+++ interface/src/avatar/MyAvatar.cpp
@@ -2497,7 +2497,6 @@ controller::Pose MyAvatar::getControllerPoseInWorldFrame(controller::Action acti
             const Rig& rig = _skeletonModel->getRig();
             int headIndex = rig.indexOfJoint("Head");
             if (tracker && !FaceTracker::isMuted() && headIndex != -1) {
-                const Head* head = getHead();
                 auto rigHeadPose = rig.getAbsoluteDefaultPose(headIndex);
                 glm::mat4 avatarMatrix = createMatFromQuatAndPos(getWorldOrientation(), getWorldPosition());
                 glm::vec3 pos = rigHeadPose.trans() + tracker->getHeadTranslation() / 10.0f;
