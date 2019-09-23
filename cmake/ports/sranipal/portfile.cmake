c81a51f18f6a41412e6198eba4a2e52ab6915ddb
Seth Alves <seth.alves@gmail.com>
2019-09-23 10:02:51 -0700

    move to new sranipal sdk

--- cmake/ports/sranipal/portfile.cmake
+++ cmake/ports/sranipal/portfile.cmake
@@ -1,13 +1,13 @@
 include(vcpkg_common_functions)
-set(SRANIPAL_VERSION 1.0.1.0)
+set(SRANIPAL_VERSION 1.1.0.1)
 set(MASTER_COPY_SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src)
 
 if (WIN32)
     vcpkg_download_distfile(
         SRANIPAL_SOURCE_ARCHIVE
-        URLS https://hifi-public.s3.amazonaws.com/seth/sranipal-1.0.1.0-windows.zip
-        SHA512 8b7cb175f33f14b02159f9215f2791f87b067869d837d1a51d7895cf880d282515860bd850ac74d3491534c92bcd4473bb7884e68a05847689c56469fb4e122d
-        FILENAME sranipal-1.0.1.0-windows.zip
+        URLS https://hifi-public.s3.amazonaws.com/seth/sranipal-1.1.0.1-windows.zip
+        SHA512 c5a339506bdac9735c98d6405816800ea8f14b4338b2f86e57e4b033e4999052154a5f9b9e9ad7ce1140e8f3d795868fd1841c70a34ded69d9b7cb26c74540de
+        FILENAME sranipal-1.1.0.1-windows.zip
     )
 
     vcpkg_extract_source_archive(${SRANIPAL_SOURCE_ARCHIVE})
